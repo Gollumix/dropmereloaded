fb633a024cf3c117f02ebf39f841d0833d0055fa
Gollumix <107421553+Gollumix@users.noreply.github.com>
2025-12-03 23:24:06 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -13,3 +13,21 @@
       sidebar.classList.remove("active");
     }
   });
+
+        (function(){
+        window.onload = function () {
+          document.getElementById('cookie-popup-dismiss-btn').addEventListener('click', dismissCookiePopup);
+          
+          var shouldShowPopup = localStorage.getItem('cookiePopupConsumed') !== '1';
+          if (shouldShowPopup) {
+            setTimeout(function () {
+              document.getElementById('cookie-popup').className += ' is-visible';
+            }, 1000);
+          }
+        }
+    
+        function dismissCookiePopup() {
+          localStorage.setItem('cookiePopupConsumed', '1');
+          document.getElementById('cookie-popup').className = '';
+        }
+      })();
